35a998684d464e30baefdfdf26e8646d453a3e7b
jkyleBlackmon <jkyle.blackmon@gmail.com>
2022-11-30 14:40:08 -0500

    commiting new camera method

--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {PureComponent, Component} from 'react';
 import {
     Text,
     View,
@@ -31,6 +31,26 @@ function CameraButton({onPress, children}) {
     );
 }
 
+function takePicture(camera){
+    const options = { quality: 0.5, base64: true };
+    const data = camera.takePicture(options);
+    //  eslint-disable-next-line
+    console.log(data.uri);
+};
+
+const PendingView  = function(){
+    <View
+    style={{
+      flex: 1,
+      backgroundColor: 'lightgreen',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Text>Connecting...</Text>
+  </View>
+}
+
 function detection() {
     //var sock = new WebSocket("ws://localhost:5001");
     var sock = new WebSocket("ws://10.0.2.15:3000");  //replace this address with the one the node.js server prints out. keep port 3000
@@ -45,12 +65,38 @@ function detection() {
 }
 
 function StartCamera() {
-   <RNCamera ref={cam => {this.camera = cam;}} 
-           style={styles.preview}
-           aspect={Camera.constants.aspect.fill} >
-        <Text onPress={this.takePicture.bind(this)} style={styles.capture}>Take Picture</Text>
-    </RNCamera>
-
+    return (
+        <View style={styles.container}>
+        <RNCamera
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+            }}
+        >
+            {({ camera, status, recordAudioPermissionStatus }) => {
+            if (status !== 'READY') return <PendingView />;
+            return (
+                <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
+                    <Text style={{ fontSize: 14 }}> SNAP </Text>
+                </TouchableOpacity>
+                </View>
+            );
+            }}
+        </RNCamera>
+        </View>
+    );
 }
 
 function Home({navigation}) {
