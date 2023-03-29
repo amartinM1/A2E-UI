cd99563c27b3bd9efd5d3a8e55b1de100ea7d80f
anthony14khoury <anthony14khoury@gmail.com>
2023-03-29 11:45:20 -0400

    speech to text functionality

--- pages/home.js
+++ pages/home.js
@@ -10,7 +10,8 @@ import zlib from 'react-zlib-js';
 import FastImage from 'react-native-fast-image'
 import ImageView from 'react-native-image-view';
 import Video from 'react-native-video';
-import Speech_Text from './speech';
+// import Speech_Text from './speech';
+import Voice from '@react-native-voice/voice';
 
 import { VLCPlayer, VlCPlayerView } from 'react-native-vlc-media-player';
 
@@ -42,7 +43,7 @@ function Button({onPress, children, toStyle, textStyle}) {
 
 function StartCamera({udp}) {
 
-      return (
+    return (
 
         <View style={{ flex: 1,
                                          justifyContent: 'center',
@@ -210,6 +211,10 @@ function Home({navigation}) {
     const [tcp_connected, setTCPConnected] = useState(false);
     const [udp_connected, setUDPConnected] = useState(false);
     const [shouldShow, setShouldShow] = useState(false);
+    const [speechResult, setSpeechResult] = useState('');
+    const [loadingSpeech, setLoadingSpeech] = useState(false);
+    const [speechButton, setSpeechButton] = useState('Start Text to Speech');
+    const [predictionsButton, setPredictionsButton] = useState('Start Predictions');
     const socket = 1;
 
     tcp_server.on('error', (error) => {
@@ -239,7 +244,6 @@ function Home({navigation}) {
                 console.log('Message sent!')
             })
         });
-
     });
 
     const fetchData = async () => {
@@ -255,6 +259,8 @@ function Home({navigation}) {
         if(!tcp_connected) {
             const server = TcpSocket.createServer(function(tcp_socket) {
                 tcp_socket.on('data', (data) => {
+                    // if notspeaking
+                    // 
                     tcp_socket.write('Echo server ' + data);
                     console.log('receieved data ' + data);
                     ReceiveData(String(data), fetchData);
@@ -272,6 +278,68 @@ function Home({navigation}) {
             setTCPServer(server);
             setTCPConnected(true);
         }
+    }, []);
+
+    const speechStartHandler = e => {
+        console.log('speechStart successful', e);
+    };
+    
+    const speechEndHandler = e => {
+        setLoadingSpeech(false);
+        console.log('stop handler', e);
+    };
+    
+    const speechResultsHandler = e => {
+        const text = e.value[0];
+        setSpeechResult(text);
+    };
+    
+    const startRecording = async () => {
+        setLoadingSpeech(true);
+        try {
+            await Voice.start('en-Us');
+        }
+        catch (error) {
+            console.log('error', error);
+        }
+    };
+    
+    const stopRecording = async () => {
+        try {
+            await Voice.stop();
+            setLoadingSpeech(false);
+        } 
+        catch (error) {
+            console.log('error', error);
+        }
+    };
+    
+    const toggleSpeechButtons = () => {
+        if(speechButton == "Start Text to Speech") {
+            startRecording()
+            setSpeechButton('Stop Text to Speech');
+        }
+        else if (speechButton == "Stop Text to Speech") {
+            stopRecording()
+            setSpeechButton('Start Text to Speech');
+        }
+    };
+    const togglePredictionButtons = () => {
+        if(predictionsButton == "Start Predictions") {
+            setPredictionsButton('Stop Predictions');
+        }
+        else if (predictionsButton == "Stop Predictions") {
+            setPredictionsButton('Start Predictions');
+        }
+    };
+
+    useEffect(() => {
+        Voice.onSpeechStart = speechStartHandler;
+        Voice.onSpeechEnd = speechEndHandler;
+        Voice.onSpeechResults = speechResultsHandler;
+        return () => {
+            Voice.destroy().then(Voice.removeAllListeners);
+        };
     }, []);
 
     const renderItem = ({item}) => (
@@ -300,20 +368,23 @@ function Home({navigation}) {
                 >
                     Settings
                 </Button>
+
+                {/* <Button 
+                    onPress={() => navigation.navigate('Speech_Text')}
+                    toStyle={styles.settings}
+                    textStyle={styles.settings_text}
+                >
+                    Speech
+                </Button> */}
             </View>
             <View style={styles.main_container}>
                 <View style={styles.left_screen}>
                     <Button 
                         onPress={() => setShouldShow(!shouldShow)}
                         toStyle={styles.button}
-                        textStyle={styles.button_text}
-                    >
+                        textStyle={styles.button_text}>
                         Start Camera
                     </Button>
-                    <Speech_Text />
-                    {/* <View style={{ flex: 1 }}>
-                        <Speech_Text />
-                    </View> */}
                     {shouldShow ?
                         (
                             <StartCamera
@@ -322,12 +393,40 @@ function Home({navigation}) {
                         ) : null}
                 </View>
                 <View style={styles.verticle_line}></View>
-                <FlatList style={styles.right_screen}
-                    data={messages}
-                    renderItem={renderItem}
-                    keyExtractor={item => item.time}
-                    removeClippedSubviews={false}
-                />
+                
+                <View style={styles.right_screen}>
+                    <View style={styles.double_buttons}>
+
+                        <TouchableOpacity onPress={togglePredictionButtons} style={styles.buttons_2}>
+                            <Text style={styles.text_2}>{predictionsButton}</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={toggleSpeechButtons} style={styles.buttons_2}>
+                            <Text style={styles.text_2}>{speechButton}</Text>
+                        </TouchableOpacity>
+                        
+                    </View>
+                    
+                    <View style={styles.textInputStyle}>
+                        <TextInput
+                            value={speechResult}
+                            multiline={true}
+                            placeholder= "say something!"
+                            style={{
+                                flex: 1,
+                                height: 50,
+                            }}
+                            onChangeText={text => setSpeechResult(text)}
+                        />
+                    </View>
+
+                    <FlatList style={styles.messages}
+                        data={messages}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.time}
+                        removeClippedSubviews={false}
+                    />
+                </View>
             </View>
         </View>
     );
@@ -401,7 +500,11 @@ const styles = StyleSheet.create({
     right_screen: {
         width: '49.8%',
         height: '94%',
-        alignSelf: 'center',
+        keyboardDismissMode: 'none', 
+    },
+    messages: {
+        width: '100%',
+        height: '94%',
         keyboardDismissMode: 'none', 
     },
     verticle_line:{
@@ -427,6 +530,44 @@ const styles = StyleSheet.create({
         fontSize: 25,
         fontWeight: '600',
         color: '#FFFFFF',
+    },
+    double_buttons:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '3%',
+        marginBottom: 10,
+    },
+    buttons_2:{
+        backgroundColor: '#04a4f4',
+        padding: 10,
+        borderRadius: 50,
+        marginHorizontal: 10,
+        width: '45%',
+        backgroundColor: '#04a4f4',
+    },
+    text_2:{
+        textAlign: 'center',
+        marginBottom: 2,
+        marginTop: 2,
+        fontSize: 20,
+        fontWeight: '600',
+        color: '#FFFFFF',
+    },
+    textInputStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        height:100,
+        borderRadius: 20,
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+        shadowOffset: {width: 0, height: 1},
+        shadowRadius: 2,
+        elevation: 2,
+        shadowOpacity: 0.4,
+        color: '#000',
     },
     body: {
         fontSize: 20,
--- pages/speech.js
+++ pages/speech.js
@@ -1,55 +1,166 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
 import Voice from '@react-native-voice/voice';
 
 const Speech_Text = () => {
-  const [isRecording, setIsRecording] = useState(false);
-  const [transcription, setTranscription] = useState('');
+  const [result, setResult] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
-  const handleStartRecording = async () => {
-    setIsRecording(true);
+  const speechStartHandler = e => {
+    console.log('speechStart successful', e);
+  };
+  const speechEndHandler = e => {
+    setLoading(false);
+    console.log('stop handler', e);
+  };
+
+  const speechResultsHandler = e => {
+    console.log("Handler");
+    const text = e.value[0];
+    setResult(text);
+  };
+
+  const startRecording = async () => {
+    setLoading(true);
     try {
-      await Voice.start('en-US');
+      await Voice.start('en-Us');
+      console.log("start");
     } catch (error) {
-      console.error(error);
+      console.log('error', error);
     }
   };
 
-  const handleStopRecording = async () => {
-    setIsRecording(false);
+  const stopRecording = async () => {
     try {
       await Voice.stop();
+      setLoading(false);
+      speechResultsHandler("Anthony");
+      console.log("stop");
     } catch (error) {
-      console.error(error);
+      console.log('error', error);
     }
   };
 
-  const handleCancelRecording = async () => {
-    setIsRecording(false);
-    try {
-      await Voice.cancel();
-    } catch (error) {
-      console.error(error);
-    }
+  const clear = () => {
+    setResult('Clear');
   };
 
-  const handleVoiceResults = (event) => {
-    setTranscription(event.value[0]);
-    console.log('Transcription:', event.value[0]);
-  };
-
-  Voice.onSpeechResults = handleVoiceResults;
-
+  useEffect(() => {
+    Voice.onSpeechStart = speechStartHandler;
+    Voice.onSpeechEnd = speechEndHandler;
+    Voice.onSpeechResults = speechResultsHandler;
+    console.log(Voice.onSpeechResults);
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+  console.log('speech recognition');
   return (
-    <>
-      {isRecording ? (
-        <button onPress={handleStopRecording}>Stop Recording</button>
-      ) : (
-        <button onPress={handleStartRecording}>Start Recording</button>
-      )}
-      <button onPress={handleCancelRecording}>Cancel Recording</button>
-      <text>{transcription}</text>
-    </>
+    <View style={styles.container}>
+      <SafeAreaView>
+        {/* <Text style={styles.headingText}>Voice to Text Recognition</Text> */}
+        <View style={styles.textInputStyle}>
+          <TextInput
+            value={result}
+            multiline={true}
+            placeholder="say something!"
+            style={{
+              flex: 1,
+              height: '100%',
+            }}
+            onChangeText={text => setResult(text)}
+          />
+        </View>
+
+        <View style={styles.btnContainer}>
+          {isLoading ? (
+            <ActivityIndicator size="large" color="black" />
+          ) : (
+            <TouchableOpacity onPress={startRecording} style={styles.speak}>
+              <Text style={{color: 'white', fontWeight: 'bold'}}>Speak</Text>
+            </TouchableOpacity>
+          )}
+
+          <TouchableOpacity style={styles.stop} onPress={stopRecording}>
+            <Text style={{color: 'white', fontWeight: 'bold'}}>Stop</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.clear} onPress={clear}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>Clear</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 24,
+  },
+  headingText: {
+    alignSelf: 'center',
+    marginVertical: 26,
+    fontWeight: 'bold',
+    fontSize: 26,
+  },
+  textInputStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: 300,
+    borderRadius: 20,
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    shadowOffset: {width: 0, height: 1},
+    shadowRadius: 2,
+    elevation: 2,
+    shadowOpacity: 0.4,
+    color: '#000',
+  },
+  speak: {
+    backgroundColor: 'black',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    borderRadius: 8,
+  },
+  stop: {
+    backgroundColor: 'red',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    borderRadius: 8,
+  },
+  clear: {
+    backgroundColor: 'black',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 15,
+  },
+  btnContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    with: '50%',
+    justifyContent: 'space-evenly',
+    marginTop: 24,
+  },
+});
 
 export default Speech_Text;
